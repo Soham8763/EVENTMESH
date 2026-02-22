2b78ccf60a993220e92f9f591745e48f01a02c61
Soham8763 <soham7857@gmail.com>
2026-02-22 17:23:36 +0530

    feat: introduce Redis-backed idempotency store and generalize Kafka producer.

--- services/worker/internal/executor/send_mail.go
+++ services/worker/internal/executor/send_mail.go
@@ -2,6 +2,7 @@ package executor
 
 import (
 	"log"
+	"time"
 
 	"eventmesh/worker/internal/model"
 )
@@ -10,5 +11,7 @@ type SendEmailExecutor struct{}
 
 func (s *SendEmailExecutor) Execute(task model.WorkflowTask) error {
 	log.Printf("sending email for execution %s\n", task.WorkflowExecutionID)
+	time.Sleep(40 * time.Second)
+	log.Printf("finished sending email for execution %s\n", task.WorkflowExecutionID)
 	return nil
 }

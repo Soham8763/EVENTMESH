87f13587a60d38b560d4917de64f18d0576954c6
Soham8763 <soham7857@gmail.com>
2026-03-17 21:22:25 +0530

    feat: implement Kafka producer with `NewProducer` and `Publish` methods.

---
+++ internal/kafka/producer.go
@@ -0,0 +1,36 @@
+package kafka
+
+import (
+	"context"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Producer struct {
+	writer *kafka.Writer
+}
+
+func NewProducer(brokers []string, topic string) *Producer {
+	return &Producer{
+		writer: &kafka.Writer{
+			Addr:     kafka.TCP(brokers...),
+			Topic:    topic,
+			Balancer: &kafka.LeastBytes{},
+		},
+	}
+}
+
+func (p *Producer) Publish(ctx context.Context, key string, value []byte) error {
+	msg := kafka.Message{
+		Key:   []byte(key),
+		Value: value,
+	}
+
+	err := p.writer.WriteMessages(ctx, msg)
+	if err != nil {
+		log.Println("kafka publish error:", err)
+	}
+
+	return err
+}
